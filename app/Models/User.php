e943f423de5f1d4f3c5ad85cdd607f69f65e73a1
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-12 19:29:50 +0200

    feat: add relation with POI table

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function pointsOfInterest()
+{
+    return $this->hasMany(PointOfInterest::class);
+}
 }
