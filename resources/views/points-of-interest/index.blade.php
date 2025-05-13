40ec883dca678958b6fcd24e3e103be155196db2
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-13 16:30:10 +0200

    feat: edit style

--- resources/views/points-of-interest/index.blade.php
+++ resources/views/points-of-interest/index.blade.php
@@ -6,37 +6,45 @@
 
     <h1>Lista dei punti di interesse</h1>
 
-    <div class="table-responsive">
+    <div class="container my-5">
+        <div class="row justify-content-center gap-4">
+            @foreach ($pointsOfInterest as $poi)
+                <div class="card px-0 shadow" style="width: 18rem;">
+                    @if ($poi->firstImage)
+                        <img src="{{ $poi->firstImage->path }}" alt="Immagine di {{ $poi->name }}">
+                    @else
+                        <img src="https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-network-placeholder-png-image_3416659.jpg"
+                            alt="Immagine mancante">
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $poi->name }}</h5>
+                        <p class="card-text clamp-2">{{ $poi->description }}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item flex flex-col">
+                            <p class="m-0">Numero telefonico:</p>
+                            <strong><a href="tel:{{ $poi->phone_number }}">{{ $poi->phone_number }}</a></strong>
+                        </li>
+                        <li class="list-group-item flex flex-col">
+                            <p class="m-0">Indirizzo E-Mail:</p>
+                            <strong><a href="mailto:{{ $poi->email }}">{{ $poi->email }}</a></strong>
+                        </li>
+                        <li class="list-group-item flex flex-col">
+                            <p class="m-0">Link sito web/social:</p>
+                            <a href="#"
+                                class="card-link d-inline-block text-truncate w-100">{{ $poi->external_link }}</a>
+                        </li>
+                    </ul>
+                    <div class="card-body text-center">
+                        <a href="#" class="card-link btn btn-warning"><i
+                                class="fa-regular fa-pen-to-square"></i></a></a>
+                        <a href="#" class="card-link btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+                    </div>
+                </div>
+            @endforeach
 
-        <table class="table table-striped table-hover ">
-            <thead>
-                <tr>
-                    <th>Nome punto di interesse</th>
-                    <th>Immagine copertina</th>
-                    <th>Link esterno</th>
-                    <th>Contatto telefonico</th>
-                    <th>E-Mail</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($pointsOfInterest as $poi)
-                    <tr>
-                        <td>{{ $poi->name }}</td>
-                        <td>
-                            @if ($poi->firstImage)
-                                <img src="{{ $poi->firstImage->path }}" width="100" alt="Immagine di {{ $poi->name }}">
-                            @else
-                                Nessuna immagine
-                            @endif
-                        </td>
-                        <td>{{ $poi->external_link }}</td>
-                        <td>{{ $poi->phone_number }}</td>
-                        <td>{{ $poi->email }}</td>
+        </div>
 
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
     </div>
 
 @endsection
