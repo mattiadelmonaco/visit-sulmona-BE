a8c6e57d734f2cbbb36f5f01b7f6beead4ee7cce
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-17 13:53:45 +0200

    feat: edit style and add homepage

--- resources/views/points-of-interest/index.blade.php
+++ resources/views/points-of-interest/index.blade.php
@@ -6,7 +6,7 @@
     <div class="container my-5">
         <div class="card shadow-sm">
             <div class="card-header bg-primary text-white d-flex justify-content-between align-items-center">
-                <h2 class="mb-0"><i class="fa-solid fa-list-ul"></i> Elenco dei punti di interesse</h2>
+                <h2 class="mb-0"><i class="fas fa-map-marker-alt me-2"></i> Elenco dei punti di interesse</h2>
                 <a href="{{ route('points-of-interest.create') }}" class="btn btn-light">
                     <i class="fas fa-plus me-2"></i>Nuovo punto di interesse
                 </a>
