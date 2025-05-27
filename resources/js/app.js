0142f068a3448511380d1c7f78339eaf27dddcb2
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-05-27 18:00:45 +0200

    edit formatting

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,5 @@
-import './bootstrap';
-import '~resources/scss/app.scss';
-import '~icons/bootstrap-icons.scss';
-import * as bootstrap from 'bootstrap';
-import.meta.glob([
-    '../img/**'
-])
+import "./bootstrap";
+import "~resources/scss/app.scss";
+import "~icons/bootstrap-icons.scss";
+import * as bootstrap from "bootstrap";
+import.meta.glob(["../img/**"]);
